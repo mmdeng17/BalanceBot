ee413041db330e4571dc583a6cbed3a6bc7aee8e
mmdeng17 <mmdeng17@gmail.com>
2016-08-16 21:36:38 -0700

    updated high-level implementation

--- BalanceBot.h
+++ BalanceBot.h
@@ -1,10 +1,23 @@
-class BalanceBot
+// interface for a reinforcement learning robot
+class IRLBot
 
 // function to get current state from sensors
-State pStateGetState();
+State pstateGetState();
+
+// function to get reward
+Reward prewGetReward(State pstateCurrState, Action pactCurrAction, State pstateNextState);
+
+// function to get the current action based on value function etc.
+Action pactGetAction(State pstateCurrState);
 
 // function to perform action based on current state/value estimate
-void PerformAction();
+void PerformOptimalAction();
+
+void PerformAction(Action pactCurrAction);
 
 // function to update value estimate
 void UpdateValueFcn();
+
+m_pqQFcn;
+m_alpha;
+m_gamma;
