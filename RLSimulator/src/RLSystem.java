e244cedc5462edc9b9b95e55ceb18a9d3cec725b
mmdeng17 <mmdeng17@gmail.com>
2016-08-17 23:54:54 -0700

    add members for number of states and actor and list of absorbing states and also functions for transitioning and reward fcns

--- RLSimulator/src/RLSystem.java
+++ RLSimulator/src/RLSystem.java
@@ -1,13 +1,32 @@
+import java.util.List;
 
 public abstract class RLSystem {
-	protected State m_currState;
-	private double m_timeStep;
+	protected State m_pstateCurrState;
+	protected int m_cNumStates;
+	protected double m_dTimeStep;
+	protected Actor m_pactorActor;
+	protected List<State> m_prgstateAbsorbingStates;
 	
+	public RLSystem() {
+		m_pstateCurrState = null;
+		m_cNumStates = 0;
+		m_dTimeStep = -1;
+		m_pactorActor = null;
+		m_prgstateAbsorbingStates = null;
+	}
+
 	public State pstateGetCurrState() {
-		return m_currState;
+		return m_pstateCurrState;
 	}
 	
-	double dTransition(Action pactCurrAction) {
+	public boolean FCanTransition(Action pactCurrAction) {
+		return false;
+	}
+	
+	public void Transition(Action pactCurrAction) {
+	}
+	
+	public double DGetRewardFcn(State pstatePrevState, Action pactCurrAction, State pStateCurrState) {
 		return 0.0;
 	}
 }
