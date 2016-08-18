145a9cf1879cc2af6a71b5f4a4556efde0f4079c
mmdeng17 <mmdeng17@gmail.com>
2016-08-17 23:48:22 -0700

    add flag for absorbing state

--- RLSimulator/src/State.java
+++ RLSimulator/src/State.java
@@ -1,4 +1,12 @@
 
 public abstract class State {
-
+	protected boolean m_fIsAbsorbing;
+	
+	public boolean FIsAbsorbing() {
+		return m_fIsAbsorbing;
+	}
+	
+	public void SetIsAbsorbing(boolean fIsAbsorbing) {
+		m_fIsAbsorbing = fIsAbsorbing;
+	}
 }
