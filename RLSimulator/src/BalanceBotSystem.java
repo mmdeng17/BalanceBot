a0a91af6b646ab8f2597be3720d77acca5c333b0
mmdeng17 <mmdeng17@gmail.com>
2016-08-17 23:56:02 -0700

    assorted refactoring

--- RLSimulator/src/BalanceBotSystem.java
+++ RLSimulator/src/BalanceBotSystem.java
@@ -4,21 +4,18 @@ public class BalanceBotSystem extends RLSystem {
 	public BalanceBotSystem() {
 		double[] rgdTmp = {1,0,0,0};
 		BBState pbbstateTmp = new BBState(rgdTmp);
-		m_currState = pbbstateTmp;
+		m_pstateCurrState = pbbstateTmp;
 	}
 	
 	public BalanceBotSystem(double[] rgdStateRg) {
 		BBState pbbstateTmp = new BBState(rgdStateRg);
-		m_currState = pbbstateTmp;
+		m_pstateCurrState = pbbstateTmp;
 	}
 	
-	double dTransition(Action pactCurrAction) {
+	public void Transition(Action pactCurrAction) {
 		double dCurrX, dCurrY, dCurrVel, dCurrAcc, dCurrAngle;
 		BBAction pbbactCurrAction;
 		
 		pbbactCurrAction = (BBAction) pactCurrAction;
-		
-		
-		return 0.0;
 	}
 }
