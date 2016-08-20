f524d8c965694f64816a93043c00866af9586e3c
mmdeng17 <mmdeng17@gmail.com>
2016-08-19 17:51:28 -0700

    add name to action definition

--- RLSimulator/src/Action.java
+++ RLSimulator/src/Action.java
@@ -1,4 +1,20 @@
 
-public abstract class Action {
-
+public class Action {
+	protected String m_stName;
+	
+	public Action() {
+		m_stName = null;
+	}
+	
+	public Action(String stName) {
+		m_stName = stName;
+	}
+	
+	public String StGetName() {
+		return m_stName;
+	}
+	
+	public void SetName(String stName) {
+		m_stName = stName;
+	}
 }
