cce0e4cc6ceeedcc814476ddf572e68d6e82f180
mmdeng17 <mmdeng17@gmail.com>
2016-08-19 17:53:28 -0700

    update how actions cause transitions in the system

--- RLSimulator/src/RLSystem.java
+++ RLSimulator/src/RLSystem.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Set;
 
 public abstract class RLSystem {
 	protected State m_pstateCurrState;
@@ -6,6 +7,7 @@ public abstract class RLSystem {
 	protected double m_dTimeStep;
 	protected Actor m_pactorActor;
 	protected List<State> m_prgstateAbsorbingStates;
+	protected Set<Action> m_prgactActionSet;
 	
 	public RLSystem() {
 		m_pstateCurrState = null;
@@ -13,6 +15,7 @@ public abstract class RLSystem {
 		m_dTimeStep = -1;
 		m_pactorActor = null;
 		m_prgstateAbsorbingStates = null;
+		m_prgactActionSet = null;
 	}
 
 	public State pstateGetCurrState() {
@@ -20,13 +23,34 @@ public abstract class RLSystem {
 	}
 	
 	public boolean FCanTransition(Action pactCurrAction) {
-		return false;
+		return m_prgactActionSet.contains(pactCurrAction);
 	}
 	
-	public void Transition(Action pactCurrAction) {
+	public boolean FTransition(Action pactCurrAction) {
+		if (!this.FCanTransition(pactCurrAction))
+			return false;
+		if (!FTransitionFcn(pactCurrAction))
+			return false;
+		return true;
+	}
+	
+	protected boolean FTransitionFcn(Action pactCurrAction) {
+		return false;
 	}
 	
 	public double DGetRewardFcn(State pstatePrevState, Action pactCurrAction, State pStateCurrState) {
 		return 0.0;
+	}
+	
+	public boolean FActionIsValid(Action pactCurrAction) {
+		return m_prgactActionSet.contains(pactCurrAction);
+	}
+	
+	public Set<Action> pactGetActionSet() {
+		return m_prgactActionSet;
+	}
+	
+	public void AddActionToSet(Action pactNewAction) {
+		m_prgactActionSet.add(pactNewAction);
 	}
 }
